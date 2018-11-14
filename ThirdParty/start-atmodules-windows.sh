e95f162d6305a1b54c07c0735d6ad191ee6eb7b3
Totte Karlsson <tottek@gmail.com>
2018-11-14 11:53:35 -0800

    Some small changes to docker startup scripts

--- ThirdParty/start-atmodules-windows.sh
+++ ThirdParty/start-atmodules-windows.sh
@@ -1,17 +1,16 @@
 #! /usr/bin/bash
-image_tag="fcollman/render-python-apps"
-container="renderapps_multchan"
+image_tag="sharmi/at_modules"
+container="atmodules"
 
 echo " ======== Building and starting Docker container: $container =============="
 echo "Building image with tag: $image_tag"
-docker build -t $image_tag ./Render-Python-Apps/  
+docker build -t  $image_tag ./at_modules/
 
 docker kill $container
-docker rm $container 
+docker rm $container
 
 echo "Starting container: $container"
-docker run -d --name $container  \
+docker run -d --name $container \
 -v e:/Documents/data:/mnt/data/  \
 -v c:/pDisk/atExplorer/ThirdParty/atPipeline:/pipeline  \
--v c:/pDisk/atExplorer/ThirdParty/Render-Python-Apps:/usr/local/render-python-apps  \
--i -t $image_tag /bin/bash
+-i -t $image_tag /bin/bash 
