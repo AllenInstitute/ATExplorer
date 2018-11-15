1b9b23d8ff4d62cf4ddbd0bc50606bb3c66b7712
Totte Karlsson <tottek@gmail.com>
2018-11-15 15:25:25 -0800

    Separating UI specific properties from ATExplorer props

---
+++ Applications/ATExplorer/source/Core/ATExplorerUIProperties.cpp
@@ -0,0 +1,60 @@
+#include <vcl.h>
+#pragma hdrstop
+#include "ATExplorerUIProperties.h"
+#include "dslLogger.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+
+namespace at
+{
+
+ATExplorerUIProperties gAU;
+
+using namespace dsl;
+ATExplorerUIProperties::ATExplorerUIProperties()
+:
+ApplicationProperties("ATExplorer", "\\Software\\Allen Institute\\ATExplorer", "0.5.0"),
+GeneralProperties(shared_ptr<IniFileProperties>(new IniFileProperties)),
+LocalCacheFolder("",""),
+LastSelectedSettingsSection("",""),
+LogLevel(lAny),
+BottomPanelHeight(205),
+ImageMagickPath(""),
+ProjectPanelWidth(100),
+LastOpenedProject(""),
+DefaultRenderPythonContainer(""),
+DefaultRenderServiceContainer(""),
+DefaultATModulesContainer(""),
+DefaultRenderServiceHost("")
+{
+    append(GeneralProperties);
+}
+
+ATExplorerUIProperties::~ATExplorerUIProperties()
+{
+    Log(lInfo) << "In ATExplorers ATExplorerUIProperties destructor..";
+}
+
+bool ATExplorerUIProperties::setupGeneralProperties()
+{
+	GeneralProperties->setIniFile(&getIniFile());
+	GeneralProperties->setSectionName("General");
+
+	GeneralProperties->add((BaseProperty*)  &BottomPanelHeight.setup( 	            "HEIGHT_OF_BOTTOM_PANEL",    	    205));
+    GeneralProperties->add((BaseProperty*)  &ProjectPanelWidth.setup( 	            "PROJECT_PANEL_WIDTH",    	   		205));
+	GeneralProperties->add((BaseProperty*)  &LogLevel.setup( 	                  	"LOG_LEVEL",    	                lAny));
+	GeneralProperties->add((BaseProperty*)  &LocalCacheFolder.setup(				"LOCAL_CACHE_FOLDER",  				"C:\\ImageCache"));
+	GeneralProperties->add((BaseProperty*)  &LastSelectedSettingsSection.setup(	  	"LAST_SELECTED_SETTINGS_SECTION",	""));
+
+    //This should be part of a project
+    GeneralProperties->add((BaseProperty*)  &ImageMagickPath.setup(   				"IMAGE_MAGICK_PATH",  				"C:\\"));
+	GeneralProperties->add((BaseProperty*)  &LastOpenedProject.setup(   			"LAST_OPENED_PROJECT",  			""));
+
+    GeneralProperties->add((BaseProperty*)  &DefaultRenderPythonContainer.setup(	"DEFAULT_RENDER_PYTHON_CONTAINER", ""));
+    GeneralProperties->add((BaseProperty*)  &DefaultRenderServiceContainer.setup(	"DEFAULT_RENDER_SERVICE_CONTAINER",""));
+    GeneralProperties->add((BaseProperty*)  &DefaultATModulesContainer.setup(		"DEFAULT_AT_MODULES_CONTAINER",    ""));
+    GeneralProperties->add((BaseProperty*)  &DefaultRenderServiceHost.setup(		"DEFUALT_RENDER_SERVICE_HOST",     ""));
+    return true;
+}
+
+}
---
+++ Applications/ATExplorer/source/Core/ATExplorerUIProperties.h
@@ -0,0 +1,50 @@
+#ifndef ATExplorerUIPropertiesH
+#define ATExplorerUIPropertiesH
+#include "dslApplicationProperties.h"
+#include "dslProperty.h"
+#include "dslIniFileProperties.h"
+#include "dslSharedPointer.h"
+#include "dslLogLevel.h"
+//---------------------------------------------------------------------------
+namespace at
+{
+
+using dsl::Property;
+using dsl::IniFileProperties;
+using dsl::LogLevel;
+
+class PACKAGE ATExplorerUIProperties : public dsl::ApplicationProperties
+{
+    public:
+                                            ATExplorerUIProperties();
+                                            ~ATExplorerUIProperties();
+        bool                                setupGeneralProperties();
+
+                                            //General Properties
+        shared_ptr<IniFileProperties> 		GeneralProperties;
+
+                                            //Automatically managed properties ============================
+        Property<string>                	LocalCacheFolder;
+        Property<int>	                    BottomPanelHeight;
+        Property<int>	                    ProjectPanelWidth;
+
+                                            //!Used to open up the Settings Treeview where it was last time
+        Property<string>                	LastSelectedSettingsSection;
+        Property<string>                	ImageMagickPath;
+
+                                            //These properties may be exposed for change in the UI ========
+        Property<LogLevel>	 	           	LogLevel;
+        Property<string>	 		        LastOpenedProject;
+
+                                            //Save default to use containers
+        Property<string>                    DefaultRenderPythonContainer;
+        Property<string>                    DefaultRenderServiceContainer;
+        Property<string>                    DefaultATModulesContainer;
+        Property<string>                    DefaultRenderServiceHost;
+};
+
+//This creates a global ATExplorerUIProperties object
+extern PACKAGE ATExplorerUIProperties gAU;
+
+}
+#endif
