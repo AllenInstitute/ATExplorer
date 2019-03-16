338adc625518f5830b47ee6d960c3ffa0b03d909
Totte Karlsson <tottek@gmail.com>
2019-03-15 17:18:31 -0700

    Cross platform coding now..

---
+++ Examples/cpp/atdata/atdata_info/main.cpp
@@ -0,0 +1,117 @@
+#pragma hdrstop
+#pragma argsused
+#include "atATIFData.h"
+#include "dslLogger.h"
+#include "atExceptions.h"
+#include "atSession.h"
+#include "atRibbon.h"
+
+using namespace dsl;
+using namespace at;
+
+//Arg1 -> AtData
+void onStartingPopulating( void* arg1, void* arg2, void* arg3)
+{
+    Log(lInfo) << "Starting populating data..";
+}
+
+//Arg1 -> AtData
+void onProgressPopulating(void* arg1, void* arg2, void* arg3)
+{
+    ATData* data = (ATData*)arg1;
+    Log(lInfo) << "Progressing populating data";
+//    if(data)
+//    {
+//        Log(lInfo) << "Processing ribbon: " <<data->getNumberOfRibbons();
+//    }
+}
+
+//Arg1 -> AtData
+void onFinishedPopulating(void* arg1, void* arg2, void* arg3)
+{
+    ATData* data = (ATData*)arg1;
+    Log(lInfo) << "Finished populating data";
+   	Log(lInfo) << data->getInfo();
+}
+
+int main()
+{
+    //Setup logging
+    LogOutput::mShowLogLevel = true;
+    gLogger.setLogLevel(lInfo);
+    gLogger.logToConsole(true);
+    gLogger.logToFile("p:\\ATProjects\\MyLog.txt");
+
+    Path dataPath("c:\\data\\M33\\");
+
+	ATData* atData(new ATIFData(dataPath));
+    atData->assignOnPopulateCallbacks(onStartingPopulating, onProgressPopulating, onFinishedPopulating);
+
+    try
+    {
+        //!Populating the data object causes a scan of folders and files
+        //!representing the data. No image data is loaded
+        atData->populate(false);
+
+        //Print some information about ribbons and sections
+        Log(lInfo) << "This is a "<<atData->getNumberOfRibbons()<<" ribbon(s) dataset";
+
+        RibbonSP ribbon = atData->getFirstRibbon();
+        while(ribbon)
+        {
+            Log(lInfo) << "There are "<<ribbon->getNumberOfSections()<<" sections in ribbon "<< ribbon->getAlias();
+            ribbon = atData->getNextRibbon();
+        }
+
+        //Check Sessions and channels, i.e. actual data
+        SessionSP session =  atData->getFirstSession();
+        while(session)
+        {
+            Log(lInfo) << "Checking session " << session->getLabel();
+
+            //Get Channels in session
+            ChannelSP ch = session->getFirstChannel();
+            while(ch)
+            {
+                Log(lInfo) << "Checking channel: " << ch->getLabel();
+                RibbonSP ribbon = atData->getFirstRibbon();
+                while(ribbon)
+                {
+                    SectionSP section = ribbon->getFirstSection();
+                    //Loop through sections
+                    while(section)
+                    {
+                        TilesSP tiles = section->getTiles(ch);
+                        if(tiles)
+                        {
+                            Log(lDebug) << "There are " << tiles->count() << " tiles in section: " << section->id()<< ", channel \"" << tiles->getChannel().getLabel() << "\" in session: "<<session->getLabel()<<" on ribbon \"" << ribbon->getAlias() << "\"";
+                        }
+                        section = ribbon->getNextSection();
+                    }
+
+                    ribbon = atData->getNextRibbon();
+                }
+                ch = session->getNextChannel();
+            }
+            session = atData->getNextSession();
+        }
+
+        Log(lInfo) << atData->getInfo();
+    }
+    catch(const FileSystemException& e)
+    {
+        Log(lError) << "Exception: " << e.what();
+        Log(lInfo) << "Application exiting..";
+    }
+    catch(...)
+    {
+
+    }
+
+	return 0;
+}
+
+#pragma comment(lib, "atExplorerFoundation")
+#pragma comment(lib, "dslFoundation")
+#pragma comment(lib, "poco_foundation")
+
--- Source/atdata/atATDataUtils.h
+++ Source/atdata/atATDataUtils.h
@@ -2,16 +2,23 @@
 #define atATDataUtilsH
 #include "atATDataExporter.h"
 #include "dslSharedPointer.h"
+#include <string>
 //---------------------------------------------------------------------------
 
 namespace at
 {
 
+using std::string;
 class Section;
 class FileFolder;
 enum  ATDataFileFormat {atAllen, atKM, atBase};
 
+#if defined(__BORLANDC_)
 typedef std::tr1::shared_ptr<FileFolder>  FileFolderSP;
+#elseif defined(__clang__)
+typedef std::shared_ptr<FileFolder>  FileFolderSP;
+#endif
+
 
 int 	  ATE_DATA		getSectionID(const string& filePath);
 int 	  ATE_DATA		getTileID(const string& filePath);
